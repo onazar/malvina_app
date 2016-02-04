ef9ffabdb2e3ac2fab7ed2476297e372f948e53a
Sasha Nazar <nazar.sasha@gmail.com>
2016-02-04 14:12:03 +0200

    Moved from sqllite to psql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160129135026) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "costumes", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
